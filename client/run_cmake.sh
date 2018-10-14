b128da589f9e2eed78c35aac897776171352befd
Marcin Malesa <mmalesa92@gmail.com>
2018-10-14 23:57:50 +0200

    Added Boost Asio library for Client. Added Boost Windows dlls to Git LFS

--- client/run_cmake.sh
+++ client/run_cmake.sh
@@ -8,5 +8,8 @@ cd build &&
 if [ $(uname -m) == "x86_64" ] ; then
 	FLAGS="-A x64"
 fi
+if [ "$1" == "DEBUG" ] ; then
+	FLAGS="$FLAGS -DBoost_DEBUG=ON"
+fi
 cmake $FLAGS ..
 
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include <boost/asio.hpp>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
