90b63094aa6a1a38533922bef97b1db310e6fd4f
Piotr Malesa <piotrek.darcur@gmail.com>
2019-01-31 23:16:12 +0100

    Deleting audio libraries (ao, mpg123).

--- server/src/AudioPlayer.cpp
+++ server/src/AudioPlayer.cpp
@@ -10,64 +10,26 @@ AudioPlayer::~AudioPlayer()
 
 }
 
-void AudioPlayer::run(AudioTrack & track)
+void AudioPlayer::run(AudioTrack & audioTrack)
 {
     init();
-    open(track);
-    getDecodingFormat();
-    setOutputFormat();
-    openOutputDevice();
-    play();
+    play(audioTrack);
     destroy();
 }
 
 void AudioPlayer::init()
 {
-    ao_initialize();
-    driver_ = ao_default_driver_id();
-    mpg123_init();
-    mh_ = mpg123_new(NULL, &err_);
-    buffer_size_ = mpg123_outblock(mh_);
-    buffer_ = (unsigned char *) malloc(buffer_size_ * sizeof(unsigned char));
+    
 }
 
-void AudioPlayer::open(AudioTrack & track)
+void AudioPlayer::play(AudioTrack & audioTrack)
 {
-    std::string path = "../data/" + track.getTitle();
-    mpg123_open(mh_, path.c_str());
+    
 }
 
-void AudioPlayer::getDecodingFormat()
-{
-    mpg123_getformat(mh_, &rate_, &channels_, &encoding_);
-}
 
-void AudioPlayer::setOutputFormat()
-{
-    format_.bits = mpg123_encsize(encoding_) * BITS;
-    format_.rate = rate_;
-    format_.channels = channels_;
-    format_.byte_format = AO_FMT_NATIVE;
-    format_.matrix = 0;
-}
-
-void AudioPlayer::openOutputDevice()
-{
-    dev_ = ao_open_live(driver_, &format_, NULL);
-}
-
-void AudioPlayer::play()
-{
-    while (mpg123_read(mh_, buffer_, buffer_size_, &done_) == MPG123_OK)
-        ao_play(dev_, (char *)buffer_, done_);
-}
 
 void AudioPlayer::destroy()
 {
-    free(buffer_);
-    ao_close(dev_);
-    mpg123_close(mh_);
-    mpg123_delete(mh_);
-    mpg123_exit();
-    ao_shutdown();
+    
 }
