b6aaf9a1bcaad76cefb1876062f34da236124cda
Marcin Malesa <mmalesa92@gmail.com>
2019-02-01 01:43:05 +0100

    Added RtAudio library Added mpg123 library back.

--- server/src/AudioPlayer.h
+++ server/src/AudioPlayer.h
@@ -1,8 +1,12 @@
 #ifndef _AUDIO_PLAYER_
 #define _AUDIO_PLAYER_
-
+#include <mpg123.h>
 #include "AudioTrack.h"
+#include "RtAudio.h"
 #include <vector>
+#include <memory>
+
+#define BITS 8
 
 class AudioPlayer
 {
@@ -15,11 +19,26 @@ public:
 private:
 
     void init();
-    void play(AudioTrack & audioTrack);
+    void open(AudioTrack & track);
+    void getDecodingFormat();
+    void play();
     void destroy();
 
-    std::vector<AudioTrack> audioTracks_;
+    std::vector<AudioTrack> tracks_;
 
+    mpg123_handle * mh_;
+    unsigned char * buffer_;
+    size_t buffer_size_;
+    size_t done_;
+    int err_;
+
+    int channels_, encoding_;
+    long rate_;
+
+    std::unique_ptr<RtAudio> rtAudio_;
+    RtAudio::StreamParameters parameters_;
+    unsigned int sampleRate_;
+    unsigned int bufferFrames_;
 };
 
 
