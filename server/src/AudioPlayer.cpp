daf8526e3271f21720254104217a496ab8462b41
Marcin Malesa <mmalesa92@gmail.com>
2019-02-02 06:02:57 +0100

    WIP Playing decoded mp3 and saw wave at the same time

--- server/src/AudioPlayer.cpp
+++ server/src/AudioPlayer.cpp
@@ -1,8 +1,8 @@
 #include "AudioPlayer.h"
+#include <cstring>
 
 AudioPlayer::AudioPlayer()
 {
-    init();
 }
 
 AudioPlayer::~AudioPlayer()
@@ -29,7 +29,7 @@ int saw(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
   return 0;
 }
 
-int mp3Callback(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
+int mp3Cb(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
          double streamTime, RtAudioStreamStatus status, void *userData)
 {
     return ((AudioPlayer*)userData)->mp3Callback(outputBuffer, inputBuffer, nBufferFrames, streamTime, status);
@@ -38,30 +38,39 @@ int mp3Callback(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrame
 int AudioPlayer::mp3Callback(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
          double streamTime, RtAudioStreamStatus status)
 {
-    while (mpg123_read(mh_, buffer_, buffer_size_, &done_) == MPG123_OK)
+    if (status)
+        std::cout << "Stream underflow detected!" << std::endl;
+    if ((nPlayedFrames_ * 2) % mp3DecoderOutputBufferSize == 0)
     {
-        unsigned char * buffer = (unsigned char *) outputBuffer;
-        
-        
-        
-        
-        for (int i = 0; i < nBufferFrames; ++i)
-            for(int j = 0; j < channels_; ++j)
-            {
-                
-            }
-        //ao_play(dev_, (char *)buffer_, done_);
+        nPlayedFrames_ = 0;
+        if (mpg123_read(mh_, mp3DecoderOutputBuffer_, mp3DecoderOutputBufferSize, &done_) != MPG123_OK)
+            return 0;
     }
+    
+    int16_t* outBuffer = static_cast<int16_t*>(outputBuffer);
+    int16_t* mp3DecoderOutputBuffer = reinterpret_cast<int16_t*>(mp3DecoderOutputBuffer_);
+
+    for (int i = 0; i < nBufferFrames; ++i)
+    {
+        *outBuffer++ = *(mp3DecoderOutputBuffer + nPlayedFrames_);
+        mp3DecoderOutputBuffer++;
+    }
+    nPlayedFrames_ += nBufferFrames; 
+
     return 0;
 }
 
 void AudioPlayer::playMP3(AudioTrack & audioTrack)
 {
-    std::string path = "../data/" + audioTrack.getTitle();
-    mpg123_open(mh_, path.c_str());
-    mpg123_getformat(mh_, &rate_, &channels_, &encoding_);
-
-    //play();
+    try {
+        rtAudio_->openStream(&parameters_, NULL, RTAUDIO_SINT16,
+                    sampleRate_, &bufferFrames_, &mp3Cb, (void*) this);
+        rtAudio_->startStream();
+    }
+    catch (RtAudioError& e) {
+        e.printMessage();
+        exit(0);
+    }
 }
 
 void AudioPlayer::playWAV(AudioTrack & audioTrack)
@@ -74,11 +83,37 @@ void AudioPlayer::playWAV(AudioTrack & audioTrack)
     }
     catch (RtAudioError& e) {
         e.printMessage();
-        exit( 0 );
+        exit(0);
     }
 }
 
-void AudioPlayer::init()
+void AudioPlayer::initMP3(AudioTrack & audioTrack)
+{
+    mpg123_init();
+    mh_ = mpg123_new(NULL, &err_);
+    mp3DecoderOutputBufferSize = mpg123_outblock(mh_);
+    std::cout << "mpg123 buffer size = " << mp3DecoderOutputBufferSize << " bytes\n";
+    mp3DecoderOutputBuffer_ = (unsigned char *) malloc(mp3DecoderOutputBufferSize * sizeof(unsigned char));
+    std::string path = "../data/" + audioTrack.getTitle();
+    mpg123_open(mh_, path.c_str());
+    mpg123_getformat(mh_, &rate_, &channels_, &encoding_);
+    nPlayedFrames_ = 0;
+    std::cout << "Mp3 rate = " << rate_ << ", channels = " << channels_ << ", encoding = " << encoding_ <<
+        ", encsize = " << mpg123_encsize(encoding_) << "\n";
+    
+    rtAudio_ = std::make_unique<RtAudio>();
+    if (rtAudio_->getDeviceCount() < 1) {
+        std::cout << "No audio devices found!\n";
+        exit(0);
+    }
+    parameters_.deviceId = rtAudio_->getDefaultOutputDevice();
+    parameters_.nChannels = channels_;
+    parameters_.firstChannel = 0;
+    sampleRate_ = rate_;
+    bufferFrames_ = 256;
+}
+
+void AudioPlayer::initWAV(AudioTrack & audioTrack)
 {
     rtAudio_ = std::make_unique<RtAudio>();
     if (rtAudio_->getDeviceCount() < 1) {
@@ -90,16 +125,11 @@ void AudioPlayer::init()
     parameters_.firstChannel = 0;
     sampleRate_ = 44100;
     bufferFrames_ = 256;
-
-    mpg123_init();
-    mh_ = mpg123_new(NULL, &err_);
-    buffer_size_ = mpg123_outblock(mh_);
-    buffer_ = (unsigned char *) malloc(buffer_size_ * sizeof(unsigned char));
 }
 
 void AudioPlayer::destroy()
 {
-    free(buffer_);
+    free(mp3DecoderOutputBuffer_);
     mpg123_close(mh_);
     mpg123_delete(mh_);
     mpg123_exit();
