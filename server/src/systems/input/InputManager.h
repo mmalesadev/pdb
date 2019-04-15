f240e88113f4865e272a3093a354bdf6a1755c55
Piotr Malesa <piotrek.darcur@gmail.com>
2019-04-15 21:34:13 +0200

    Enabled date current date reading.

--- server/src/systems/input/InputManager.h
+++ server/src/systems/input/InputManager.h
@@ -9,7 +9,7 @@ namespace Pdb
 class InputManager
 {
 public:
-    enum Button { BUTTON_Q, BUTTON_W, BUTTON_E, BUTTON_S, BUTTON_X, BUTTON_UP, BUTTON_DOWN, BUTTON_F, BUTTON_T};
+    enum Button { BUTTON_Q, BUTTON_W, BUTTON_E, BUTTON_S, BUTTON_X, BUTTON_UP, BUTTON_DOWN, BUTTON_F, BUTTON_T, BUTTON_D};
 
     InputManager();
     void init();
